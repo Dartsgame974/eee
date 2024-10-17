4aeac6c62a790713d48dacd8a056553ef775774b
Dartsgame <80755313+Dartsgame974@users.noreply.github.com>
2024-10-17 12:36:52 +0200

    Update drive.lua

--- drive.lua
+++ drive.lua
@@ -1,13 +1,14 @@
 -- Load AUKit for audio playback
 local aukit = require("aukit")
 local speaker = peripheral.find("speaker")
+local mf = require("morefonts") -- Load More Fonts library
 
 -- Initialize peripherals and variables
 local monitor = peripheral.find("monitor")
 local rsBridge = peripheral.find("rsBridge")
 
 -- Set monitor scale and clear
-monitor.setTextScale(1.5)
+monitor.setTextScale(1)
 monitor.clear()
 
 -- Format large numbers into K (thousand), M (million), etc.
@@ -19,14 +20,6 @@ local function formatNumber(num)
     else
         return tostring(num)
     end
-end
-
--- Function to center text on the monitor
-local function centerText(text, y)
-    local w, h = monitor.getSize()
-    local x = math.floor((w - #text) / 2)
-    monitor.setCursorPos(x, y)
-    monitor.write(text)
 end
 
 -- Function to draw a progress bar using colored squares
@@ -45,11 +38,46 @@ local function drawProgressBar(used, total, width)
             -- Gray for the free storage
             monitor.setBackgroundColor(colors.gray)
         end
-        monitor.write(" ") -- Draws a colored "square" (as close as we can get in text)
+        monitor.write(" ") -- Draws a colored "square"
     end
     
     -- Reset the background color
     monitor.setBackgroundColor(colors.black)
+end
+
+-- Function to display a large centered title using More Fonts
+local function displayTitle(text)
+    monitor.setTextColor(colors.blue)
+    mf.writeOn(monitor, text, nil, 1, {
+        font = "fonts/PublicPixel", -- Choose a font here
+        scale = 2, -- Make it larger
+        anchorHor = "center",
+    })
+end
+
+-- Function to display the stockage complet warning
+local function displayWarning()
+    local w, h = monitor.getSize()
+
+    -- Play the warning sound for 45 seconds
+    local startTime = os.clock()
+    while os.clock() - startTime <= 45 do
+        -- Flickering text in the center
+        monitor.clear()
+        monitor.setTextColor(colors.red)
+        mf.writeOn(monitor, "STOCKAGE COMPLET!!!!", nil, math.floor(h / 2), {
+            font = "fonts/PublicPixel", -- Choose a bold font
+            scale = 2,
+            anchorHor = "center",
+        })
+
+        sleep(0.5) -- Flicker every half second
+        monitor.clear()
+        sleep(0.5)
+
+        -- Play alert.wav in the background
+        aukit.play(aukit.stream.wav(io.lines("alert.wav", 48000)), speaker)
+    end
 end
 
 -- Main function to display storage information
@@ -64,60 +92,28 @@ local function displayStorage()
 
     local freeStorage = totalStorage - usedStorage
 
-    -- Clear the monitor
+    -- Clear the monitor and display title
     monitor.clear()
-
-    -- Display centered title
-    monitor.setTextColor(colors.blue)
-    centerText("Google Drive", 1)
+    displayTitle("Google Drive")
 
     -- Draw the progress bar centered
     local w, h = monitor.getSize()
-    drawProgressBar(usedStorage, totalStorage, w - 2) -- Use most of the width
+    drawProgressBar(usedStorage, totalStorage, w - 2)
 
     -- Display the shortened storage numbers below the bar
     local storageText = formatNumber(usedStorage) .. " / " .. formatNumber(totalStorage)
     monitor.setCursorPos(1, 5)
     monitor.setTextColor(colors.green)
-    centerText(storageText, 5)
+    mf.writeOn(monitor, storageText, nil, 5, { anchorHor = "center" })
 
     -- Display the free space centered
     local freeText = "Free: " .. formatNumber(freeStorage)
     monitor.setTextColor(colors.lightGray)
-    centerText(freeText, 7)
+    mf.writeOn(monitor, freeText, nil, 7, { anchorHor = "center" })
 
     -- Check if free storage is less than 10,000 and display warning with sound loop
     if freeStorage <= 10000 then
-        monitor.clear()
-        local flicker = true
-        local startTime = os.clock()
-        
-        -- Loop the sound for 45 seconds
-        while freeStorage <= 10000 do
-            -- Play the sound in a loop while flickering text
-            if os.clock() - startTime <= 45 then
-                aukit.play(aukit.stream.wav(io.lines("alert.wav", 48000)), speaker)
-            end
-
-            -- Centered flickering full storage message
-            monitor.setCursorPos(1, math.floor(h / 2))
-            if flicker then
-                monitor.setTextColor(colors.red)
-                centerText("STOCKAGE COMPLET!!!!", math.floor(h / 2))
-            else
-                monitor.clearLine()
-            end
-            flicker = not flicker
-            sleep(0.5)
-            
-            -- Update storage status to break out of the loop if space is freed
-            usedStorage = 0
-            for _, item in pairs(rsBridge.listItems()) do
-                usedStorage = usedStorage + item.amount
-            end
-            freeStorage = totalStorage - usedStorage
-        end
-        monitor.clear()
+        displayWarning()
         displayStorage() -- Re-display storage info when space is freed
     end
 end
